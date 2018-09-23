0e5f8bf961e22e95921a4a0f094ec339757beebb
mazureq <39035883+mazureq@users.noreply.github.com>
2018-09-23 20:49:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://hidden-beach-72572.herokuapp.com//v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://hidden-beach-72572.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
